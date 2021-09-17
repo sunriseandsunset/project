c2f6cfbcd7bd955b5f797d7a21cd359a7265b52e
LAPTOP-JD27JJ00\so-l0 <karus1500@gmail.com>
2021-09-17 19:06:46 +0900

    time-test

--- app.py
+++ app.py
@@ -1,47 +1,26 @@
-from pymongo import MongoClient
-
-from flask import Flask, render_template, jsonify, request
-
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-db = client.dbsparta
-
-
-# HTML 화면 보여주기
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
+@app.route('/codes', methods=['GET'])
+def test_get():
+   title_receive = request.args.get('group')
+   print(title_receive)
+   return jsonify({'group':'success', 'msg': '이 요청은 GET!'})
 
-# API 역할을 하는 부분
-@app.route('/api/list', methods=['GET'])
-def show_stars():
-    movie_star = list(db.mystar.find({}, {'_id': False}).sort('like',-1))
-    return jsonify({'movie_stars': movie_star})
+   doc = { 'market': ''
 
+   }
 
-@app.route('/api/like', methods=['POST'])
-def like_star():
-    name_receive = request.form['name_give']
+@app.route('/test', methods=['POST'])
+def test_post():
+   title_receive = request.form['title_give']
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
 
-    target_star = db.mystar.find_one({'name':name_receive})
-    current_like = target_star['like']
-
-    new_like = current_like + 1
-
-    db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
-
-    return jsonify({'msg': '좋아요 완료!'})
-
-
-@app.route('/api/delete', methods=['POST'])
-def delete_star():
-    name_receive = request.form['name_give']
-    db.mystar.delete_one({'name':name_receive})
-    return jsonify({'msg': '삭제 완료!'})
-
-# 수정수정수정
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0',port=5000,debug=True)
