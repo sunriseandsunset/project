d6ed0d37c12538162e7f64c8fd0b6bd6a0d51c23
dduoming <dltndudvldzm@naver,com>
2021-10-17 18:20:30 +0900

    클라이언트와 서버 간 쿠키연결 , 마이페이지 구현

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import flask
-from flask import Flask, render_template, jsonify, request,redirect,url_for
+from flask import Flask, render_template, jsonify, request,redirect,url_for,flash
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
 import json
 import xmltodict
 import sys
-<<<<<<< HEAD
+
 from datetime import datetime
-=======
 import jwt
 import datetime
 import hashlib
@@ -18,7 +17,7 @@ from datetime import datetime, timedelta
 
 
 
->>>>>>> origin/main_sparta
+
 
 app = Flask(__name__)
 
@@ -30,18 +29,21 @@ db = client.dbproject
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
-    # kimp = flask.Respo
-    # nse()
-    # kimp.headers["Access-Control-Allow-Origin"] = "*"
     return render_template('1page2-.html')
-=======
-    return render_template('1page2-.html')
+
+@app.route('/index')
+def index2():
+    #return render_template('1page2-.html')
     token_receive = request.cookies.get('mytoken')
+    print(token_receive)
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         user_info = db.users.find_one({"username": payload["id"]})
-        return render_template('1page2-.html', user_info=user_info)
+        usernane=user_info['username']
+        print(usernane)
+
+        return render_template('1page2-.html', username=usernane,token=token)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
@@ -64,7 +66,7 @@ def sign_in():
     if result is not None:
         payload = {
          'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=5)  # 로그인 24시간 유지
+         'exp': datetime.utcnow() + timedelta(seconds=400)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
@@ -109,10 +111,17 @@ def check_dup():
 @app.route('/mypage', methods=['GET'])
 def mypage():
     token_receive = request.cookies.get('mytoken')
+    print('mypage 서버에서 받는 토큰',token_receive)
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         user_info = db.users.find_one({"username": payload["id"]})
-        return render_template('mypage.html', user_info=user_info)
+        username=user_info['username']
+        reviews = list(db.reviews.find({"writer": username}, {'_id': False}).sort([("reg_date", -1)]))
+        #사용자가 찜한 하트보기
+        heart_place = list(db.heart.find({"username": username}, {'_id': False}).sort([("reg_date", -1)]))
+        print(heart_place)
+        return render_template('mypage.html', username=username,reviews=reviews,heart_place=heart_place,token=token)
 
     except (jwt.exceptions.DecodeError,jwt.ExpiredSignatureError):
         return redirect(url_for("/"))
@@ -120,22 +129,46 @@ def mypage():
 
 
 
->>>>>>> origin/main_sparta
 
 
 #지역별 명소 데이터 api 가져오기
 @app.route('/place',methods=['GET','POST'])
 def show_place():
+    token_receive = request.cookies.get('mytoken')
+    print('2page 서버에서 받는 토큰', token_receive)
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        user_info = db.users.find_one({"username": payload["id"]})
 
-    #선택한지역 넘겨주기
-    if request.method == 'POST':
-        want = request.form.get('inputGroupSelect04')
-        #print(want)
-        places = db.sample.find({"area": want}, {'_id': False})
+        # 선택한지역 넘겨주기
+        if request.method == 'POST':
+            want = request.form.get('inputGroupSelect04')
+            # print(want)
+            places = db.sample.find({"area": want}, {'_id': False})
 
-        places=list(places)
+            places = list(places)
+            return render_template('2page-swiper''.html', data=want, places=places,token=token)
+    except (jwt.exceptions.DecodeError,jwt.ExpiredSignatureError):
+        return redirect(url_for("/"))
 
-        return render_template('2page.html',data=want,places=places)
+    #     return render_template('2page-swiper''.html', data=want, places=places)
+
+    # token_receive = request.cookies.get('search_token')
+    # try:
+    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    #
+    #     #선택한지역 넘겨주기
+    #     if request.method == 'POST':
+    #         want = request.form.get('inputGroupSelect04')
+    #         #print(want)
+    #         places = db.sample.find({"area": want}, {'_id': False})
+    #
+    #         places=list(places)
+    #
+    #         return render_template('2page-swiper''.html',data=want,places=places)
+    # except:
+
 
 #명소의 자세한 페이지
 @app.route('/detail',methods=['POST','GET'])
@@ -151,19 +184,47 @@ def show_detail():
 
 @app.route('/3page',methods=['GET'])
 def show_detail2():
-     name = request.args.get('name')
-     #명소에 대한 정보 불러오기
-     places = db.sample.find({"name": name}, {'_id': False})
-     #명소에 대한 리뷰 불러오기
-     # reviews= db.reviews.find({"where": name}, {'_id': False})
-     #최신리뷰 3개만 3페이지에 나타나게
-     reviews = list(db.reviews.find({"where": name}, {'_id': False}).sort([("reg_date", -1)]))
-     places=list(places)
-     reviews=list(reviews)
-     if len(reviews)>3:
-         reviews=reviews[0:3]
-     print(reviews)
-     return render_template('3page-.html',place=places,reviews=reviews)
+     token_receive=request.cookies.get('mytoken')
+     print('3페이지에서받은토큰',token_receive)
+     try:
+         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+         name = request.args.get('name')
+         print(name)
+         #명소에 대한 정보 불러오기
+         places = db.sample.find({"name": name}, {'_id': False})
+         #명소에 대한 리뷰 불러오기
+         # reviews= db.reviews.find({"where": name}, {'_id': False})
+         #최신리뷰 3개만 3페이지에 나타나게
+         reviews = list(db.reviews.find({"where": name}, {'_id': False}).sort([("reg_date", -1)]))
+         places=list(places)
+         reviews=list(reviews)
+         if len(reviews)>3:
+             reviews=reviews[0:3]
+
+         #명소의 하트수세기
+         heart_count=db.heart.count_documents({"place_name": name, "type": "heart"})
+         #내가 하트 했는지
+         heart_user=bool(db.heart.find_one({"place_name": name, "type": "heart", "username": payload['id']}))
+
+
+         return render_template('3page-.html',place=places,reviews=reviews,place_heart=heart_count,heart_user=heart_user)
+
+     except :
+
+         # name = request.args.get('name')
+         # print(name)
+         # # 명소에 대한 정보 불러오기
+         # places = db.sample.find({"name": name}, {'_id': False})
+         # # 명소에 대한 리뷰 불러오기
+         # # reviews= db.reviews.find({"where": name}, {'_id': False})
+         # # 최신리뷰 3개만 3페이지에 나타나게
+         # reviews = list(db.reviews.find({"where": name}, {'_id': False}).sort([("reg_date", -1)]))
+         # places = list(places)
+         # reviews = list(reviews)
+         # if len(reviews) > 3:
+         #     reviews = reviews[0:3]
+         # flash('로그인을 해주세요!')
+         return render_template('login.html')
 
 #공공데이터 api query url 만드는함수
 def get_request_query(url, operation, params, serviceKey):
@@ -200,46 +261,73 @@ def show_time():
 
 @app.route('/reviewpage', methods=['GET'])
 def go_reviews():
-    place = request.args.get('place')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        place = request.args.get('place')
+        want_reviews = list(db.reviews.find({'where':place}, {'_id': False}))
+        return render_template('review.html', reviews=want_reviews,place=place)
+    except:
 
-    want_reviews = list(db.reviews.find({'where':place}, {'_id': False}))
-
-    return render_template('review.html', reviews=want_reviews,place=place)
-
-
+        return render_template('login.html')
 
 ## API 역할을 하는 부분
 @app.route('/reviewpage', methods=['POST'])
 def write_review():
-    title_receive=request.form['title_give']
-    where_receive = request.form['where_give']
-    upload_receive = request.form['upload_give']
-    print(upload_receive)
-    review_receive = request.form['review_give']
-    image_url='https://sparta-forposting.s3.ap-northeast-2.amazonaws.com/'+upload_receive
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        # 어떤 명소인지
+        username=user_info['username']
+        title_receive=request.form['title_give']
+        where_receive = request.form['where_give']
+        upload_receive = request.form['upload_give']
+        print(upload_receive)
+        review_receive = request.form['review_give']
+        image_url='https://sparta-forposting.s3.ap-northeast-2.amazonaws.com/'+upload_receive
 
-    doc = {
-        'where':where_receive,
-        'title':title_receive,
-        'upload':image_url,
-        'review':review_receive,
-        'reg_date': datetime.now(),
-    }
-    db.reviews.insert_one(doc)
+        doc = {
+            'writer':username,
+            'where':where_receive,
+            'title':title_receive,
+            'upload':image_url,
+            'review':review_receive,
+            'reg_date': datetime.now(),
+        }
+        db.reviews.insert_one(doc)
 
-    return jsonify({'msg': '저장완료!'})
-
-#
-# @app.route('/reviewpage', methods=['GET'])
-# def read_reviews():
-#     go_reviews = list(db.reviews.find({}, {'_id': False}))
-#
-#     return jsonify({'all_reviews': go_reviews})
+        return jsonify({'msg': '저장완료!'})
+    except:
+        return render_template('login.html')
 
 
+@app.route('/heart',methods=['post'])
+def update_heart():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        #어떤 명소인지
+        place_receive = request.form["place_give"]
+        type_receive = request.form["type_give"]
+        #즐겨찾기 취소인지 실행인지
+        action_receive = request.form["action_give"]
+        doc = {
+            "place_name": place_receive,
+            "username": user_info["username"],
+            "type": type_receive
+        }
+        if action_receive == "like":
+            db.heart.insert_one(doc)
+        else:
+            db.heart.delete_one(doc)
+
+        count = db.heart.count_documents({"place_name": place_receive, "type": type_receive})
+        return jsonify({"result": "success", 'msg': 'updated', "count": count})
 
 
-
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000,debug=True)
