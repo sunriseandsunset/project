521f16b99448b0a950e4a6a59f9295d9627dec4e
LAPTOP-JD27JJ00\so-l0 <karus1500@gmail.com>
2021-10-19 10:21:59 +0900

    수정

--- application.py
+++ application.py
@@ -16,11 +16,9 @@ import hashlib
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 
-
 application = Flask(__name__)
 cors = CORS(application, resources={r"/*": {"origins": "*"}})
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
+SECRET_KEY = (os.environ.get("SECRET_KEY"))
 
 # client = MongoClient('localhost', 27017)
 client = MongoClient(os.environ.get("MONGO_DB_PATH"))
